e77b2a40b22554d49f7a3a86c1ec7ede3679b182
Oladele Tijani <jaeswave@gmail.com>
2026-09-16 11:57:31 +0100

    added new main

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://api-syncmono.onrender.com/api'
 });
 
 export default api;
+
+
+
